5a247c6536a23b0573123b31b2325da47be77b75
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-13 19:12:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,12 @@
 def key_for_min_value(name_hash)
   min_value = nil
   return nil if !name_hash
-  else name_hash.each do |key, value|
-      if !min_value
+  name_hash.each do |key, value|
+    if !min_value
+      min_value = key
+    elsif name_hash[key] < min_value
         min_value = key
-      elsif name_hash[key] < min_value
-          min_value = key
-        end
+    end
       end
     end
   end
