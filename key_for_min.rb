e539804a910b47973cf7c3e99bf89c1fd839ed5f
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-13 19:11:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,9 +9,9 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       if !min_value
         min_value = key
-      else
-        if name_hash[key] < min_value
+      elsif name_hash[key] < min_value
           min_value = key
+
       end
     end
   end
